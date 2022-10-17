998a59f4122d3040a3d721628955f6a0e78e33f4
Daniel Firth <dan.firth@homotopic.tech>
2022-10-17 15:55:54 +0100

    fourmolu: 0.8.2.0 -> c6d7156b5f3e8287960a76e777c40b62ebfdbcb3

--- pkgs/fourmolu.nix
+++ pkgs/fourmolu.nix
@@ -1,95 +1,34 @@
-{ mkDerivation
-, aeson
-, ansi-terminal
-, array
-, base
-, bytestring
-, Cabal
-, containers
-, Diff
-, directory
-, dlist
-, exceptions
-, filepath
-, ghc-lib-parser
-, gitrev
-, hspec
-, hspec-discover
-, hspec-megaparsec
-, lib
-, megaparsec
-, MemoTrie
-, mtl
-, optparse-applicative
-, path
-, path-io
-, pretty
-, process
-, QuickCheck
-, syb
-, template-haskell
-, temporary
-, text
-, th-lift-instances
-, yaml
+{ mkDerivation, aeson, ansi-terminal, array, base, bytestring
+, Cabal, containers, Diff, directory, dlist, exceptions, fetchgit
+, filepath, ghc-lib-parser, gitrev, hspec, hspec-discover
+, hspec-megaparsec, lib, megaparsec, MemoTrie, mtl
+, optparse-applicative, path, path-io, pretty, process, QuickCheck
+, syb, template-haskell, temporary, text, th-lift-instances, yaml
 }:
 mkDerivation {
   pname = "fourmolu";
   version = "0.8.2.0";
-  sha256 = "3dfa8569f16f5aa12f43442e25d1bce13e291b526b2ea941be005ce8fc90cef3";
+  src = fetchgit {
+    url = "https://github.com/fourmolu/fourmolu";
+    sha256 = "0y4gdwimyvh6ha86hwjp62b9cnjwsb0svd3jcwjjaffcikwalbvs";
+    rev = "c6d7156b5f3e8287960a76e777c40b62ebfdbcb3";
+    fetchSubmodules = true;
+  };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson
-    ansi-terminal
-    array
-    base
-    bytestring
-    Cabal
-    containers
-    Diff
-    directory
-    dlist
-    exceptions
-    filepath
-    ghc-lib-parser
-    megaparsec
-    MemoTrie
-    mtl
-    syb
-    template-haskell
-    text
-    th-lift-instances
-    yaml
+    aeson ansi-terminal array base bytestring Cabal containers Diff
+    directory dlist exceptions filepath ghc-lib-parser megaparsec
+    MemoTrie mtl syb template-haskell text th-lift-instances yaml
   ];
   executableHaskellDepends = [
-    base
-    containers
-    directory
-    filepath
-    ghc-lib-parser
-    gitrev
-    optparse-applicative
-    text
-    yaml
+    base containers directory filepath ghc-lib-parser gitrev
+    optparse-applicative text yaml
   ];
   testHaskellDepends = [
-    base
-    containers
-    Diff
-    directory
-    filepath
-    ghc-lib-parser
-    hspec
-    hspec-megaparsec
-    megaparsec
-    path
-    path-io
-    pretty
-    process
-    QuickCheck
-    temporary
-    text
+    base containers Diff directory filepath ghc-lib-parser hspec
+    hspec-megaparsec megaparsec path path-io pretty process QuickCheck
+    temporary text
   ];
   testToolDepends = [ hspec-discover ];
   doHaddock = false;
