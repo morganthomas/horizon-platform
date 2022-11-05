ca6f9db8c403c4791c4114b46f4df30ce8c5fc6f
Daniel Firth <dan.firth@homotopic.tech>
2022-11-05 14:35:34 +0000

    data-sketches-core: init at 0.1.0.0

--- overlay.nix
+++ overlay.nix
@@ -191,6 +191,8 @@ final: prev: with pkgs.haskell.lib; {
 
   data-fix = prev.callPackage (./pkgs/data-fix.nix) { };
 
+  data-sketches-core = prev.callPackage (./pkgs/data-sketches-core.nix) { };
+
   dec = prev.callPackage (./pkgs/dec.nix) { };
 
   dense-linear-algebra = prev.callPackage (./pkgs/dense-linear-algebra.nix) { };
