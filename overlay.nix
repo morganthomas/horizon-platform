13148d14ba1e84a590077226a3e755297fe0647f
Daniel Firth <dan.firth@homotopic.tech>
2022-11-05 12:53:16 +0000

    libsodium: init at 1.0.18.2

--- overlay.nix
+++ overlay.nix
@@ -441,6 +441,8 @@ final: prev: with pkgs.haskell.lib; {
 
   lens-family-th = prev.callPackage (./pkgs/lens-family-th.nix) { };
 
+  libsodium = prev.callPackage (./pkgs/libsodium.nix) { };
+
   libyaml = prev.callPackage (./pkgs/libyaml.nix) { };
 
   lifted-async = prev.callPackage (./pkgs/lifted-async.nix) { };
