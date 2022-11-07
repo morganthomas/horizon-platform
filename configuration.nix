07685e0dfa5dab0c2f9059c5198af51490b6c1eb
Daniel Firth <dan.firth@homotopic.tech>
2022-11-07 17:25:12 +0000

    saltine: init at 0.2.0.1

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,8 @@ with pkgs.haskell.lib;
 
 final: prev: {
 
+  saltine = addPkgconfigDepend prev.saltine pkgs.libsodium;
+
   libsodium = prev.callPackage ./pkgs/libsodium.nix { inherit (pkgs) libsodium; };
 
 }
---
+++ pkgs/saltine.nix
@@ -0,0 +1,34 @@
+{ mkDerivation, base, bytestring, criterion, deepseq, hashable, lib
+, libsodium, profunctors, QuickCheck, semigroups, test-framework
+, test-framework-quickcheck2, text
+}:
+mkDerivation {
+  pname = "saltine";
+  version = "0.2.0.1";
+  sha256 = "ce6867a08a308bc5155cb4c5a146837cf6c5e056dd0d0151f9f0838b2eed2ea0";
+  isLibrary = true;
+  isExecutable = false;
+  enableSeparateDataOutput = false;
+  libraryHaskellDepends = [
+    base bytestring deepseq hashable profunctors text
+  ];
+  libraryPkgconfigDepends = [ libsodium ];
+  testHaskellDepends = [
+    base bytestring QuickCheck semigroups test-framework
+    test-framework-quickcheck2 text
+  ];
+  benchmarkHaskellDepends = [
+    base bytestring criterion deepseq text
+  ];
+  benchmarkSystemDepends = [ libsodium ];
+  enableLibraryProfiling = false;
+  enableExecutableProfiling = false;
+  doHaddock = false;
+  jailbreak = true;
+  doCheck = false;
+  doBenchmark = false;
+  hyperlinkSource = false;
+  description = "Cryptography that's easy to digest (NaCl/libsodium bindings)";
+  license = lib.licenses.mit;
+  broken = false;
+}
