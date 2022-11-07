6eacda3ca0ca87ba5ba149bd9732d142d15f4fcb
Daniel Firth <dan.firth@homotopic.tech>
2022-11-07 11:29:01 +0000

    stringbuilder: init at 0.5.1

--- overlay.nix
+++ overlay.nix
@@ -931,6 +931,8 @@ self: with pkgs.haskell.lib; {
 
   string-qq = self.callPackage (./pkgs/string-qq.nix) { };
 
+  stringbuilder = self.callPackage (./pkgs/stringbuilder.nix) { };
+
   swagger2 = self.callPackage (./pkgs/swagger2.nix) { };
 
   syb = self.callPackage (./pkgs/syb.nix) { };
