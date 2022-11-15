9d0b00c8ce7e5b53960b2702fb7edc4c9668ac7c
Daniel Firth <dan.firth@homotopic.tech>
2022-11-15 03:22:15 +0000

    be more explicit about legacyPackages

--- flake.nix
+++ flake.nix
@@ -8,13 +8,18 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
+
   outputs = inputs@{ self, nixpkgs, flake-utils, get-flake, horizon-gen-nix, lint-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+
+        pkgs = nixpkgs.legacyPackages.${system};
+
         horizon-gen-nix-app = get-flake horizon-gen-nix;
+
         haskellLib = pkgs.haskell.lib.compose;
-        hsPkgs = pkgs.callPackage (nixpkgs + /pkgs/development/haskell-modules) {
+
+        legacyPackages = pkgs.callPackage (nixpkgs + /pkgs/development/haskell-modules) {
           buildHaskellPackages = pkgs.haskell.packages.ghc942;
           compilerConfig = pkgs.callPackage ./configuration-ghc-9.4.x.nix { inherit haskellLib; };
           configurationCommon = import ./configuration.nix;
@@ -24,16 +29,20 @@
           initialPackages = import ./overlay.nix;
           nonHackagePackages = self: super: { };
         };
+
         packages = pkgs.lib.filterAttrs
           (n: v: v != null
             && builtins.typeOf v == "set"
             && pkgs.lib.hasAttr "type" v
             && v.type == "derivation"
             && v.meta.broken == false)
-          hsPkgs;
+          legacyPackages;
+
         horizon-gen-gitlab-ci = pkgs.writers.writeBashBin "gen-gitlab-ci" "${pkgs.dhall-json}/bin/dhall-to-yaml --file .gitlab-ci.dhall";
+
       in
       {
+
         apps = {
           horizon-gen-nix = horizon-gen-nix-app.outputs.apps.${system}.horizon-gen-nix;
           horizon-gen-gitlab-ci = {
@@ -41,11 +50,15 @@
             program = "${horizon-gen-gitlab-ci}/bin/gen-gitlab-ci";
           };
         };
+
         checks = {
           dhall-format = lint-utils.outputs.linters.x86_64-linux.dhall-format ./.;
           nixpkgs-fmt = lint-utils.outputs.linters.x86_64-linux.nixpkgs-fmt ./.;
         };
-        legacyPackages = hsPkgs;
+
+        inherit legacyPackages;
+
         inherit packages;
+
       });
 }
