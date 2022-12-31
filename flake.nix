5606c3b16649defd5e3b0ca2bad6f559b994f2a6
Daniel Firth <dan.firth@homotopic.tech>
2022-12-31 14:06:17 +0000

    remove horizon-gen-gitlab-ci app

--- flake.nix
+++ flake.nix
@@ -56,8 +56,6 @@
             && v.meta.broken == false)
           legacyPackages;
 
-        horizon-gen-gitlab-ci = writeBashBin "gen-gitlab-ci" "${pkgs.dhall-json}/bin/dhall-to-yaml --file .gitlab-ci.dhall";
-
         run-impure-tests = writePorcelainOrDieBin {
           name = "run-impure-tests";
           src = ./.;
@@ -84,11 +82,6 @@
           horizon-gen-nix = {
             type = "app";
             program = "${horizon-gen-nix}/bin/horizon-gen-nix";
-          };
-
-          horizon-gen-gitlab-ci = {
-            type = "app";
-            program = "${horizon-gen-gitlab-ci}/bin/gen-gitlab-ci";
           };
 
           procex = {
